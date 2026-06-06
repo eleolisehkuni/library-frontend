1d96ecb8c7f6904f81ede7f33d617cb66529f6be
eleolisehkuni <ezuluj@gmail.com>
2026-06-05 21:36:56 -0400

    Add App component with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600 underline">
-      Hello, Tailwind + Vite!
-    </h1>
-  )
+    <Router>
+      <div className="max-w-4xl mx-auto p-4">
+        
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/add" element={<BookForm />} />
+          <Route path="/edit/:id" element={<BookForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
